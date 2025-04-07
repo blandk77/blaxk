72a87c37d383b96d3978718378ff0fa9705e77df
blandk77 <alexags1983@gmail.com>
2025-04-07 21:34:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, Message
 from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 import yt_dlp
 import asyncio
